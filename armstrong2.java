66ec61bac836d91967a4ca2af7d2f481b2217a9c
kanneboyinasujatha <41731075+kanneboyinasujatha@users.noreply.github.com>
2018-08-01 14:11:33 +0530

    Create armstrong2.java

---
+++ armstrong2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		 int n, count = 0, a, b, c, sum = 0;
+        System.out.print("Armstrong numbers from 1 to 1000:");
+        for(int i = 1; i <= 1000; i++)
+        {
+            n = i;
+            while(n > 0)
+            {
+                b = n % 10;
+                sum = sum + (b * b * b);
+                n = n / 10;
+            }
+            if(sum == i)
+            {
+                System.out.print(i+" ");
+            }
+            sum=0;
+        }
+        
+	}
+}
